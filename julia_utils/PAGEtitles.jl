392c8965af1937b115bc8b24bf9c30d1a259829c
Martin Alfaro <50346244+alfaromartino@users.noreply.github.com>
2023-06-13 23:07:17 -0600

    paths

--- julia_utils/PAGEtitles.jl
+++ julia_utils/PAGEtitles.jl
@@ -1,3 +1,18 @@
+############################################################################
+#
+#               FOLDER CONTAINING .MD FILES
+#
+############################################################################
+path_prepath = "blog"
+subfolder    = "PAGES"
+
+
+############################################################################
+#
+#                           BASIC INFORMATION
+#
+############################################################################
+
 using DataFrames
 
 function previous_folder(path; nr_prev=1) 
@@ -17,43 +32,50 @@ end
 
 
 
-folder = joinpath(previous_folder(@__DIR__), "PAGES")
-
-files   = readdir(folder)  |>  x -> x[startswith.(x, r"^\d")]  |>   #we keep those files that start with a number
-          x-> sort!(x, rev=true)                                    #we sort them in descending order
-titles  = capture_PAGEdescription.(folder, files,"PAGEpost_name")
-dates   = capture_PAGEdescription.(folder, files,"PAGEpost_date")
-updates = capture_PAGEdescription.(folder, files,"PAGEpost_update")
-
-pagemd   = string.(["PAGES\\"], files)
-
-files    = replace.(files, r".md$" => "") #we remove .md
-pagelink = string.(["\\PAGES\\"], files)
-
 
 function createTitle(file, title, date, update)
     toadd0(date)   = string("""<span style="font-size:70%">$(date)</span>""", " ")
-    toadd1(file)   = string("<li><a href=\"", "/", "PAGES/", "$(file)", "/", "\"",">")
+    toadd1(file)   = string("<li><a href=\"", "/", "$(subfolder)/", "$(file)", "/", "\"",">")
     toadd2(title)  = string("$(title)",raw"""</a><br>""")
     toadd3(update) = string(ifelse("$(update)" .== "", "", """<span style="font-size:70%">(updated on $(update))</span>"""), raw"""</li><br>""")
 
     string(toadd1(file), toadd2(title), " ", toadd0(date), toadd3(update))
 end
 
+## function that creates the variable for config
 
-PAGEStoiter = string.(createTitle.(files, titles, dates, updates))
+function PAGElist_and_dftoc(subfolder)
+    folder = joinpath(previous_folder(@__DIR__), "$(subfolder)")
+
+    files   = readdir(folder)  |>  x -> x[startswith.(x, r"^\d")]  |>   #we keep those files that start with a number
+            x-> sort!(x, rev=true)                                    #we sort them in descending order
+    titles  = capture_PAGEdescription.(folder, files,"PAGEpost_name")
+    dates   = capture_PAGEdescription.(folder, files,"PAGEpost_date")
+    updates = capture_PAGEdescription.(folder, files,"PAGEpost_update")
+
+    pagemd   = string.(["$(subfolder)\\"], files)
+
+    files    = replace.(files, r".md$" => "") #we remove .md
+    pagelink = string.(["$(subfolder)\\"], files)
+
+    PAGEStoiter = string.(createTitle.(files, titles, dates, updates))
 
 
-function dftoc_pages(files)
     chapter_nr  = eachindex(files)
     dftoc       = DataFrame(number=chapter_nr, name = files, pagemd = pagemd, pagelink = pagelink, 
                             title=titles, date=dates, update=updates)
+    return PAGEStoiter, dftoc
 end
 
-dftoc = dftoc_pages(files)
+PAGEStoiter, dftoc = PAGElist_and_dftoc(subfolder)
 
 
-# TO CREATE THE NEXT AND PREVIOUS PAGE
+############################################################################
+#
+#                           TO CREATE THE NEXT AND PREVIOUS PAGE
+#
+############################################################################
+
 function add_nextAndPrevious!(dftoc)
     prev_page = [dftoc[i+1, :pagemd] for i in 1:nrow(dftoc)-1]
     push!(prev_page,"")
@@ -69,3 +91,24 @@ add_nextAndPrevious!(dftoc)
 
 dftoc[lastindex(dftoc.pagemd), :prev_page]  = replace(dftoc.pagemd[1], r".md$" => "")
 dftoc[firstindex(dftoc.pagemd), :next_page] = replace(dftoc.pagemd[end], r".md$" => "")
+
+
+############################################################################
+#
+#                           TO CREATE ASSETS LOCATION
+#
+############################################################################
+
+
+
+# TO REFER TO ASSETS (for images and the link to codeDownload)
+    assets_folder(PAGEfile) = "/assets/$(subfolder)/$(PAGEfile)"
+dftoc.path_assets       = assets_folder.(dftoc.name)
+dftoc.temp_assets       = string.("/$(path_prepath)", dftoc.path_assets)
+
+let dftoc = dftoc
+    assets_folder(PAGEfile) = "/assets/$(subfolder)/$(PAGEfile)"
+    html_tree               = "https://github.com/alfaromartino/$(path_prepath)/tree/main/"
+    dftoc.path_codeDownload = string.(html_tree, dftoc.path_assets, "/codeDownload")
+end
+
